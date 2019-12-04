b62c3265bef246735a468c8637ad804a137f4b14
leonardogarupa <leonardo.sa@garupa.co>
2019-12-04 14:07:52 -0300

    controller de dev, e model

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const server = express();
 
+mongoose.connect('mongodb://localhost/test');
+
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
