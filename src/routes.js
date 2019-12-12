cb58ba55c587b5f4587875a65c17be09068e77ee
leonardogarupa <leonardo.sa@garupa.co>
2019-12-12 12:59:08 -0300

    rota do index para cards

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const routes = express.Router();
 const CardController = require("./controllers/CardController");
 const UserController = require("./controllers/UserController");
 
-routes.get('/', CardController.index);
+routes.get('/cards', CardController.index);
 routes.post('/cards', CardController.store);
 routes.put('/cards/:id/edit', CardController.update);
 routes.delete("/cards/:id/delete", CardController.destroy);
